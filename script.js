f9addc9771004b675ef906d3564996809052afb2
yadav76 <93885474+yadav76@users.noreply.github.com>
2023-04-02 01:02:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 function calculateMinCost() {
   //your code here
+  // read data from input element;
   var inputData = document.querySelector("#rope-lengths").value;
+  var inputArr = inputData.split(",");
 
-	var inputArr = inputData.split(",");
-    // alert(inputArr);
+  for (var i = 0; i < inputArr.length; i++) {
+    inputArr[i] = Number(inputArr[i]);
+  }
+  var cost = 0;
 
-	// Now convert array of string to array of number
-	for (var i=0; i<inputArr.length; i++) {
-		inputArr[i] = Number(inputArr[i]);
-	}
+  while (inputArr.length > 1) {
+    var newRope = inputArr[0] + inputArr[1];
+    cost += newRope;
 
-	if (inputArr.length ==1) cost = inputArr[0];
+    // delete first two element
+    inputArr.splice(0, 2);
+    inputArr.push(newRope);
+    //alert(newRope);
+  }
+  document.querySelector("#result").textContent = cost;
+}
 
-	// Now sort the array
-	inputArr.sort(function(a,b) {return a-b});
+// function calculateMinCost() {
+//   //your code here
+//   var inputData = document.querySelector("#rope-lengths").value;
 
-	var cost = 0;
-    while (inputArr.length > 1) {
-		var rope = inputArr[0] + inputArr[1];
-		cost += rope;
+// 	var inputArr = inputData.split(",");
+//     // alert(inputArr);
 
-		// now delete 1st 2 Ele from input array
-		inputArr.splice(0,2,rope);
+// 	// Now convert array of string to array of number
+// 	for (var i=0; i<inputArr.length; i++) {
+// 		inputArr[i] = Number(inputArr[i]);
+// 	}
 
-		inputArr.sort(function(a,b) {return a-b});
-	}
+// 	if (inputArr.length ==1) cost = inputArr[0]
 
-	//alert(inputArr);
-	// Now show result on screen
-	document.querySelector("#result").textContent = cost;
-}  
+// 	// Now sort the array
+// 	inputArr.sort(function(a,b) {return a-b});
+
+// 	var cost = 0;
+//     while (inputArr.length > 1) {
+// 		var rope = inputArr[0] + inputArr[1];
+// 		cost += rope;
+
+// 		// now delete 1st 2 Ele from input array
+// 		inputArr.splice(0,2,rope);
+
+// 		inputArr.sort(function(a,b) {return a-b});
+// 	}
+
+// 	//alert(inputArr);
+// 	// Now show result on screen
+// 	document.querySelector("#result").textContent = cost;
+// }  
