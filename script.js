0a5ab6201924c759552440a2620ccbb360fda100
yadav76 <93885474+yadav76@users.noreply.github.com>
2023-04-02 00:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function calculateMinCost() {
   //your code here
-  var inputData = document.querySelector("rope-lengths").value;
+  var inputData = document.querySelector("#rope-lengths").value;
 
-alert(inputData);
-  
-  
+	var inputArr = inputData.split(",");
+    // alert(inputArr);
+
+	// Now convert array of string to array of number
+	for (var i=0; i<inputArr.length; i++) {
+		inputArr[i] = Number(inputArr[i]);
+	}
+
+	// Now sort the array
+	inputArr.sort(function(a,b) {return a-b});
+
+	var cost = 0;
+    while (inputArr.length > 1) {
+		var rope = inputArr[0] + inputArr[1];
+		cost += rope;
+
+		// now delete 1st 2 Ele from input array
+		inputArr.splice(0,2,rope);
+
+		inputArr.sort(function(a,b) {return a-b});
+	}
+
+	//alert(inputArr);
 }  
